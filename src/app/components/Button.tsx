c14c7eb9b4bffb5532c928d25a6c61cf8195b324
SaiKG <saikumar.gurugu.com>
2025-05-06 21:33:59 +1000

    button

--- src/app/components/Button.tsx
+++ src/app/components/Button.tsx
@@ -2,9 +2,9 @@
 import React from "react";
 import { Button as MuiButton } from "@mui/material";
 
-export default function Button({ children, onClick }: { children: React.ReactNode; onClick: () => void }) {
+export default function Button({ children, onClick, className }: { children: React.ReactNode; onClick: () => void; className?: string }) {
   return (
-    <MuiButton variant="contained" color="primary" onClick={onClick}>
+    <MuiButton variant="contained" color="primary" className={className} onClick={onClick}>
       {children}
     </MuiButton>
   );
