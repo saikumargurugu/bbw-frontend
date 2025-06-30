4458007421ea5b0ac1d3af2edcd8746f163fc7f4
SaiKG <saikumar.gurugu.com>
2025-06-30 18:35:30 +1000

    fix

--- src/app/[[...slug]]/page.tsx
+++ src/app/[[...slug]]/page.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { type FC } from 'react';
 import Club from '@/app/pages/club';
 import Contact from '@/app/pages/contact';
 import Services from '@/app/pages/services';
@@ -9,37 +9,43 @@ import SignUp from '../pages/signUp';
 import ShopPage from '@/app/pages/shop/pages/index';
 import ProductDetailPage from '../pages/shop/products/details/page';
 
-export default function Page({ params }: { params: { slug?: string[] } }) {
-    const slugs = params.slug || [];
-    // Handle /shop/products/1/details
-    if (
-        slugs[0] === 'shop' &&
-        slugs[1] === 'products' &&
-        slugs[3] === 'details'
-    ) {
-        const productId = slugs[2];
-        return <ProductDetailPage productId={productId} />;
-    }
-
-    // Handle top-level routes
-    switch (slugs[0]) {
-        case 'club':
-            return <Club />;
-        case 'contact':
-            return <Contact />;
-        case 'services':
-            return <Services />;
-        case 'court-hire':
-            return <CourtHirePage />;
-        case 'shop':
-            return <ShopPage />;
-        case 'academy':
-            return <AcademyPage />;
-        case 'socials':
-            return <SocialsPage />;
-        case 'sign-up':
-            return <SignUp />;
-        default:
-            return <div>404 Not Found</div>;
-    }
+interface PageProps {
+  params: { slug?: string[] };
 }
+
+const Page: FC<PageProps> = ({ params }) => {
+  const slugs = params.slug || [];
+  // Handle /shop/products/1/details
+  if (
+    slugs[0] === 'shop' &&
+    slugs[1] === 'products' &&
+    slugs[3] === 'details'
+  ) {
+    const productId = slugs[2];
+    return <ProductDetailPage productId={productId} />;
+  }
+
+  // Handle top-level routes
+  switch (slugs[0]) {
+    case 'club':
+      return <Club />;
+    case 'contact':
+      return <Contact />;
+    case 'services':
+      return <Services />;
+    case 'court-hire':
+      return <CourtHirePage />;
+    case 'shop':
+      return <ShopPage />;
+    case 'academy':
+      return <AcademyPage />;
+    case 'socials':
+      return <SocialsPage />;
+    case 'sign-up':
+      return <SignUp />;
+    default:
+      return <div>404 Not Found</div>;
+  }
+};
+
+export default Page;
