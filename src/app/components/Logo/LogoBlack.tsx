618232812a4ac5b875244cde963ba22f5a590ba3
SaiKG <saikumar.gurugu.com>
2025-07-12 15:47:21 +1000

    moving images to public folder

--- src/app/components/Logo/LogoBlack.tsx
+++ src/app/components/Logo/LogoBlack.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import Image from "next/image";
-
+import logoMinBlack from "@/public/logos/logoMinBlack.svg";
+import logoFullBlack from "@/public/logos/logoFullBlack.svg";
   export default function LogoBlack({ variant = "full" }: { variant?: "min" | "full" }) {
   return (
       <Image
-        src={variant === "min" ? "/logos/logoMinBlack.svg" : "/logos/logoFullBlack.svg"}
+        src={variant === "min" ? logoMinBlack : logoFullBlack}
         alt="Brisbane Badmintion"
         width={32} // Adjust width as needed
         height={32} // Adjust height as needed
