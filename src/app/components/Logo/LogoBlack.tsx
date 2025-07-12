4f417b8bae7cd58a78b06cb59f5b93938e08f7be
SaiKG <saikumar.gurugu.com>
2025-07-12 15:47:46 +1000

    images for the logo component

--- src/app/components/Logo/LogoBlack.tsx
+++ src/app/components/Logo/LogoBlack.tsx
@@ -1,15 +1,14 @@
 import React from "react";
 import Image from "next/image";
-import logoMinBlack from "@/public/logos/logoMinBlack.svg";
-import logoFullBlack from "@/public/logos/logoFullBlack.svg";
-  export default function LogoBlack({ variant = "full" }: { variant?: "min" | "full" }) {
+
+export default function LogoBlack({ variant = "full" }: { variant?: "min" | "full" }) {
   return (
-      <Image
-        src={variant === "min" ? logoMinBlack : logoFullBlack}
-        alt="Brisbane Badmintion"
-        width={32} // Adjust width as needed
-        height={32} // Adjust height as needed
-        className="h-8 w-auto object-contain"
-      />
+    <Image
+      src={variant === "min" ? "/logos/logoMinBlack.svg" : "/logos/logoFullBlack.svg"}
+      alt="Brisbane Badminton"
+      width={32}
+      height={32}
+      className="h-8 w-auto object-contain"
+    />
   );
 }
