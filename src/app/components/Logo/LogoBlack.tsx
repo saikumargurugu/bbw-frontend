8eb503931ba273b0779332ce223ab8a56562c50b
SaiKG <saikumar.gurugu.com>
2025-07-12 16:07:21 +1000

    FIX LOGO BLACK

--- src/app/components/Logo/LogoBlack.tsx
+++ src/app/components/Logo/LogoBlack.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 export default function LogoBlack({ variant = "full" }: { variant?: "min" | "full" }) {
   return (
     <Image
-      src={variant === "min" ? "/logos/logoMinBlack.svg" : "/logos/logoFullBlack.svg"}
+      src={variant === "min" ? "/logos/LogoMinBlack.svg" : "/logos/LogoFullBlack.svg"}
       alt="Brisbane Badminton"
       width={32}
       height={32}
