2c8e4d5c74dd3ef7a83afa6cc4fd85702997b595
SaiKG <saikumar.gurugu@gmail.com>
2025-12-07 12:21:51 +1000

    Refactor configuration files for improved readability and organization

--- next.config.ts
+++ next.config.ts
@@ -7,9 +7,6 @@ const nextConfig: NextConfig = {
     domains: ["www.shutterstock.com", "images.unsplash.com", "storage.googleapis.com"], // Add the external domain here
     unoptimized: true,
   },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
 };
 
 export default nextConfig;
